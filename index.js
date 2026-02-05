35d21318cf644e79e0fdc81c6694697b077193ac
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-05 14:44:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function taxable(state,taxExempt){
+  if (IA,taxExempt === false) {
+return 'is taxable'
+  }
+}
+function isVehicle(hasWheels,canFly,canSwim){
+  if (true);
+    return 'is vehicle'
+  if (hasWheels === true, canFly, canSwim === false);
+}
+}
+return 'is vehicle' 
+if (canFly === true, hasWheels, canSwim === false);
+return 'is vehicle'
+if (canSwim, === true, hasWheels, canFly === false);
+return 'is vehicle'
+if (canSwim, canFly === true, hasWheels === false);
 
